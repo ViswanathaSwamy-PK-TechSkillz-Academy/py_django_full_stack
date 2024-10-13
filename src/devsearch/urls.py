1a2b8060ef4dbca6e7d7e6d7579fdb36bf5d69f3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-13 13:24:02 +0530

    Update urls.py

--- src/devsearch/urls.py
+++ src/devsearch/urls.py
@@ -15,9 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def projects(request):
+    return HttpResponse('List of Projects')
+
+
+def project(request):
+    return HttpResponse('A Single Project')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('projects/', projects, name='projects'),
+    path('project/', project, name='project'),
 ]
