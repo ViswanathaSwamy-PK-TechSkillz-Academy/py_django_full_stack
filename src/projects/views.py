845aedcde69ca57adcddc376ca5453ad740a7112
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-14 22:08:50 +0530

    templates inside respective apps

--- src/projects/views.py
+++ src/projects/views.py
@@ -6,9 +6,9 @@ from django.shortcuts import render
 
 #  /projects/ -> List of Projects
 def projects(request):
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 
 # /project/1/ -> Single Project
 def project(request, pk):
-    return render(request, 'single-project.html')
+    return render(request, 'projects/single-project.html')
